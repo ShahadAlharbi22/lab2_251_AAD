d1a61ce0c7625b93abc1356900674ea93c301a70
Shahad Alharbi <salharbi0996@stu.kau.edu.sa>
2024-01-29 11:30:14 +0300

    simple print statement

---
+++ src/pkg251/lab2/shahadalharbi/pkg2206721/Lab2ShahadAlharbi2206721.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg251.lab2.shahadalharbi.pkg2206721;
+
+/**
+ *
+ * @author 2206721
+ */
+public class Lab2ShahadAlharbi2206721 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("hello into github");
+        // TODO code application logic here
+    }
+    
+}
